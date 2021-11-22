abc9d5a285112f51f1090788735a9ef74c499a5f
zikam-KVD <67856116+zikam-KVD@users.noreply.github.com>
2021-11-22 14:32:27 +0100

    add CKEditor

---
+++ CKEditor/post_data.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>POST request</title>
+    </head>
+    <body>
+        <?php
+            var_dump($_POST);
+        ?>
+    </body>
+</html>
